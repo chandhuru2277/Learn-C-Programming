1afcd78b6ca72b17887dab8ef8e2df0f12df9bc2
root <root@LAPTOP-4Q8OND7F.localdomain>
2025-05-29 08:07:16 +0000

    hello world.c

---
+++ 1. Starting program/hello-world.c
@@ -0,0 +1,12 @@
+// HEADER FILE
+#include <stdio.h>
+
+// MAIN FUNCTION
+int main(void)
+{
+    // PRINT "Hello world"
+    printf("\nHello world\n");
+
+    // SIGNAL TO THE OS FOR THIS PROGRAM RUN SUCCESSFULLY
+    return 0;
+}
