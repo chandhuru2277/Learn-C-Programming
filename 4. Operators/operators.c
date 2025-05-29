f65c1985bee3c383a28f2f867fe326662cba31b4
root <root@LAPTOP-4Q8OND7F.localdomain>
2025-05-29 12:16:41 +0000

    operators.c

---
+++ 4. Operators/operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 100, b = 20;
+    printf("\nAdd: %d\n", a + b);
+    printf("\nSub: %d\n", a - b);
+    printf("\nMul: %d\n", a * b);
+    printf("\nDiv: %d\n", a / b);
+    printf("\nModulo: %d\n", a % b);
+    
+    return 0;
+}
