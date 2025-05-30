1e048cbbe2c67a521caa3b2453f51e3b0727dcad
root <root@LAPTOP-4Q8OND7F.localdomain>
2025-05-30 14:28:47 +0000

    string.c

---
+++ 12. Strings/basic-string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // STATIC METHOD
+    char str1[6] = {'H', 'e', 'l', 'l', 'o', '\0'}; // include '\0', so length is 6
+    printf("\nstr1: %s\n", str1);
+
+    char str2[6] = "Hello"; // include '\0' , so length is 6
+    printf("\nstr2: %s\n", str2);
+
+    // DYNAMIC METHOD (without mention size)
+    char str3[] = "Hello"; // character array
+    printf("\nstr3: %s\n", str3);
+
+    return 0;
+}
