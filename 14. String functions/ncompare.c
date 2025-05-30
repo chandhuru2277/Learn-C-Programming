08f99b274cd9347011b9a9ff3e6198fd812eee47
root <root@LAPTOP-4Q8OND7F.localdomain>
2025-05-30 15:52:09 +0000

    strncmp

---
+++ 14. String functions/ncompare.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str1[] = "Hello world";
+    char *str2 = "Hello World";
+
+    int status = strncmp(str1, str2, 7);
+    if (status == 0)
+        printf("\nMatch\n");
+    else if (status > 0)
+        printf("\nstr1 is higher than str 2\n");
+    else
+        printf("\nstr1 is lower than str 2\n");
+
+    return 0;
+}
