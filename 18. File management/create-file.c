22557d4e4a8ea9b7ceef1ed99d5929e8262784b5
root <root@LAPTOP-4Q8OND7F.localdomain>
2025-05-31 13:16:25 +0000

    create-file

--- 18. File management/create-file.c
+++ 18. File management/create-file.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    FILE *fp = fopen("./file_1.txt", "w");
+    if (fp == NULL)
+    {
+        perror("file open error");
+    }
+    else
+    {
+        char buffer[100];
+        printf("\nEnter the message:");
+        fgets(buffer, 100, stdin);
+        fprintf(fp, "%s", buffer);
+
+        fclose(fp);
+    }
     return 0;
 }
