9185cb8d0279174f1ec3d3f70addefabfd9e9be6
root <root@LAPTOP-4Q8OND7F.localdomain>
2025-06-01 05:08:16 +0000

    format

--- 18. File management/fseek-ftell.c
+++ 18. File management/fseek-ftell.c
@@ -1,32 +1,32 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    FILE *fp= fopen("./file_4.txt", "r");
-    if(fp==NULL)
+    FILE *fp = fopen("./file_4.txt", "r");
+    if (fp == NULL)
     {
         perror("file is error");
     }
     else
     {
-        
-        int pos= ftell(fp);
+
+        int pos = ftell(fp);
         printf("\ncurrent position: %d\n", pos);
 
-        fseek(fp,5,SEEK_CUR);
-        pos= ftell(fp);
+        fseek(fp, 5, SEEK_CUR);
+        pos = ftell(fp);
         printf("\nset current position (5): %d\n", pos);
 
         fseek(fp, 2, SEEK_SET);
-        pos= ftell(fp);
+        pos = ftell(fp);
         printf("\nset position (2): %d\n", pos);
 
         fseek(fp, -2, SEEK_END);
-        pos= ftell(fp);
+        pos = ftell(fp);
         printf("\nmove cursor -2 reverse order (-2): %d\n", pos);
 
-        char ch= fgetc(fp);
+        char ch = fgetc(fp);
         printf("\ncharacter: %c\n", ch);
 
         fclose(fp);
